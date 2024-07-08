37f46d4a164ccbfb2e7ac65c2605ee687eaf2432
Aatika hakim <aatikahakim90@gmail.com>
2024-07-08 11:17:39 +0500

    added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 # Day 1
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -29,5 +30,20 @@ async def item(item_id: int):
     return {'item': item_id}
 
 # Day-2
+class Cars(str, Enum):
+    audi = "Audi"
+    bmw = "BMW"
+    mercedes = "Mercedes"
 
-
+@app.get('/cars/{car_names}')
+async def get_car(car_names: Cars):
+    if car_names.value == car_names.audi:
+        return {'car': car_names, 'message': "New Brands"}
+    
+    
+    if car_names.value == 'BMW':
+        return {'car': car_names, 'message': "My new car"}
+    
+    
+    if car_names.value == 'Mercedes':
+        return {'car': car_names, 'message': "Happy driving"}
